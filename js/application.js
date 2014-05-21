f75185eb279e5f19f383517e957ee222aa3f0d2d
Eric Tan <20925931@student.uwa.edu.au>
2014-05-21 22:07:07 +0800

    Skeleton set up, home page logo created
    Ready to be deployed to Heroku for testing

---
+++ js/application.js
@@ -0,0 +1,12 @@
+/* The function to be called on successful body load */
+$(function () {
+	
+});
+
+$(document).ready(function () {
+	$('#logodiv').fadeIn(2000, loadBar);
+});
+
+function loadBar() {
+	$('header').show(1000);
+}
