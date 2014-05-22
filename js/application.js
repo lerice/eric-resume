1e7d38d6f8d4c333e89664ca938c7637a5f1509f
Eric Tan <20925931@student.uwa.edu.au>
2014-05-23 03:17:00 +0800

    Footer button hover text scripts implemented

--- js/application.js
+++ js/application.js
@@ -3,10 +3,35 @@
 	
 });*/
 
-$(document).ready(function () {
-	//$('header').fadeIn(2000);
+$(document).ready(function() {
 	$('header').addClass('headeronload');
 	$('footer').addClass('footeronload');
+	
+	//Setting the individual text changes:
+	$('#nav_skills').hover(function() {
+		$('#hovertext').html('technical skills');
+	});
+	$('#nav_education').hover(function() {
+		$('#hovertext').html('education & training');
+	});
+	$('#nav_achievements').hover(function() {
+		$('#hovertext').html('achievements');
+	});
+	$('#nav_workexp').hover(function() {
+		$('#hovertext').html('work experience');
+	});
+	$('#nav_portfolio').hover(function() {
+		$('#hovertext').html('portfolio');
+	});
+	$('#nav_github').hover(function() {
+		$('#hovertext').html('my github');
+	});
+	
+	//And setting text to blank on mouseexit:
+	$('.botnavimg').hover(function() {
+	}, function() {
+		$('#hovertext').html('');
+	});
 });
 
 
@@ -18,7 +43,7 @@ function fadeIn(obj) {
 			$('#text_wd').fadeIn(500)});
 	}
 	
-	$("a.transition").click(function(event) {
+	$('a.transition').click(function(event) {
 		event.preventDefault();
 		linkLocation = this.href;
 		
