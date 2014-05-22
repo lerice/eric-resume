faee7733a4ef171fd3aaca2b4196a1f8aa527807
Eric Tan <20925931@student.uwa.edu.au>
2014-05-22 19:51:57 +0800

    Header and footer transitions implemented

--- js/application.js
+++ js/application.js
@@ -5,15 +5,32 @@
 
 $(document).ready(function () {
 	//$('header').fadeIn(2000);
+	$('header').addClass('headeronload');
+	$('footer').addClass('footeronload');
 });
 
 
 function fadeIn(obj) {
-	$(obj).fadeIn(1000,loadTitles);
+	$(obj).fadeIn(1000, loadTitles);
 	
 	function loadTitles() {
 		$('#text_et').fadeIn(500,function() {
 			$('#text_wd').fadeIn(500)});
 	}
+	
+	$("a.transition").click(function(event) {
+		event.preventDefault();
+		linkLocation = this.href;
+		
+		$('header').removeClass('headeronload');
+		$('footer').removeClass('footeronload');
+		
+		setTimeout(function() {
+			redirectPage();
+		}, 1500);
+	});
+	
+	function redirectPage() {
+		window.location = linkLocation;
+	}
 }
-
