89d9c4d714c56cf3753c2e80d70d0b2ea735440b
Eric Tan <20925931@student.uwa.edu.au>
2014-05-28 02:12:40 +0800

    Timeline transitions done, more type ups

--- js/application.js
+++ js/application.js
@@ -64,10 +64,14 @@ function fadeInHome() {
 
 // 'Fade' in the timeline entries one by one
 function fadeInWorkExp() {
-	$('#timeline_tutor').fadeIn(1000,function() {
-		$('#timeline_myer').fadeIn(1000,function() {
-			$('#timeline_crown').fadeIn(1000,function() {
-				$('#timeline_next').fadeIn(1000)
+	$('#timeline_bg').fadeIn(1000,function() {
+		$('#timeline_grad').fadeIn(1000,function() {
+			$('#timeline_tutor').fadeIn(1000,function() {
+				$('#timeline_myer').fadeIn(1000,function() {
+					$('#timeline_crown').fadeIn(1000,function() {
+						$('#timeline_next').fadeIn(1000)
+					});
+				});
 			});
 		});
 	});
